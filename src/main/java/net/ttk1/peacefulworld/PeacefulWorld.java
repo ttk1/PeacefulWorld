e2ed00e065c31259368d2a92091715c4e46efc5c
Masanari Matsuoka <m@mtsk.info>
2018-03-03 23:23:14 +0900

    close #21

--- src/main/java/net/ttk1/peacefulworld/PeacefulWorld.java
+++ src/main/java/net/ttk1/peacefulworld/PeacefulWorld.java
@@ -4,9 +4,12 @@
 
 package net.ttk1.peacefulworld;
 
+import com.google.inject.Inject;
+import com.google.inject.Injector;
 import net.ttk1.peacefulworld.api.History;
 import net.ttk1.peacefulworld.api.HistoryManager;
 import net.ttk1.peacefulworld.history.HistoryManagerImpl;
+import net.ttk1.peacefulworld.listener.BlockPlaceEventListener;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,6 +21,9 @@ import java.util.logging.Logger;
  * @author ttk1 and mmtsk
  */
 public class PeacefulWorld extends JavaPlugin {
+
+    @Inject private BlockPlaceEventListener blockPlaceEventListener;
+
     private HistoryManager historyManager;
     private FileConfiguration conf;
     private Logger logger;
@@ -29,6 +35,11 @@ public class PeacefulWorld extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        PeacefulWorldBindModule module = new PeacefulWorldBindModule(this);
+        Injector injector = module.createInjector();
+        injector.injectMembers(this);
+
         // 設定ファイルの読み込み
         conf = getConfig();
 
