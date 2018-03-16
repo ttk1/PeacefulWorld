5fa37eecec649161cc6f15848e6e97f40309c088
Masanari Matsuoka <m@mtsk.info>
2018-03-17 00:19:39 +0900

    staticファクトリーメソッド(of)の作成

--- src/main/java/net/ttk1/peacefulworld/history/BlockAdapterImpl.java
+++ src/main/java/net/ttk1/peacefulworld/history/BlockAdapterImpl.java
@@ -3,12 +3,20 @@ package net.ttk1.peacefulworld.history;
 import net.ttk1.peacefulworld.api.BlockAdapter;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 
+/**
+ * @author ttk1 and mmtsk
+ */
 public class BlockAdapterImpl implements BlockAdapter {
     private final Location loc;
     private final Material type;
     private final int typeId;
     private final byte data;
+
+    public BlockAdapterImpl(Block block) {
+        this(block.getLocation(), block.getType(), block.getTypeId(), block.getData());
+    }
 
     public BlockAdapterImpl(Location loc, Material type, int typeId, byte data){
         this.loc = loc;
